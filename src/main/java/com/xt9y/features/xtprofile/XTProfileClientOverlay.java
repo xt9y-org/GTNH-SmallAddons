c61dd525b523e7484e5561a240226f0640f824e9
xt9y <felix@xt9y.de>
2026-09-15 17:00:10 +0200

    fix: use GL12 rescale-normal constant

--- src/main/java/com/xt9y/features/xtprofile/XTProfileClientOverlay.java
+++ src/main/java/com/xt9y/features/xtprofile/XTProfileClientOverlay.java
@@ -19,6 +19,7 @@ import net.minecraftforge.client.event.MouseEvent;
 import net.minecraftforge.common.MinecraftForge;
 
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 
 import appeng.api.util.NamedDimensionalCoord;
 import appeng.client.gui.implementations.GuiCraftingCPU;
@@ -227,7 +228,7 @@ public final class XTProfileClientOverlay implements INEIGuiHandler {
         GL11.glDisable(GL11.GL_DEPTH_TEST);
         GL11.glDisable(GL11.GL_CULL_FACE);
         GL11.glDisable(GL11.GL_FOG);
-        GL11.glDisable(GL11.GL_RESCALE_NORMAL);
+        GL11.glDisable(GL12.GL_RESCALE_NORMAL);
         GL11.glEnable(GL11.GL_TEXTURE_2D);
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
