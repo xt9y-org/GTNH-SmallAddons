0f72cbca446af132760f0b09c035980076c01ea8
xt9y <felix@xt9y.de>
2026-09-16 18:27:53 +0200

    test: cover partial parallel machine timing overlap

--- src/test/java/com/xt9y/features/xtprofile/XTProfileRouteTimeAccountingTest.java
+++ src/test/java/com/xt9y/features/xtprofile/XTProfileRouteTimeAccountingTest.java
@@ -42,6 +42,41 @@ class XTProfileRouteTimeAccountingTest {
     }
 
     @Test
+    void partiallyOverlappingOperationsOnlyChargeNewMachineTicks() {
+        XTProfileData.MediumRecord medium = new XTProfileData.MediumRecord();
+        XTProfileIntervalUnion all = new XTProfileIntervalUnion();
+        XTProfileIntervalUnion cpu = new XTProfileIntervalUnion();
+
+        XTProfileRouteTracker.accountCompletedMachineTicks(
+            medium,
+            1L,
+            "medium:assline",
+            "machine:assline",
+            10L,
+            50L,
+            8_000L,
+            all,
+            cpu);
+        XTProfileRouteTracker.accountCompletedMachineTicks(
+            medium,
+            1L,
+            "medium:assline",
+            "machine:assline",
+            40L,
+            60L,
+            4_000L,
+            all,
+            cpu);
+
+        assertEquals(2_500_000_000L, medium.busyNs);
+        assertEquals(50L, medium.activeTicks);
+        assertEquals(10_000L, medium.tickCostNs);
+        assertEquals(2_500_000_000L, medium.busyNsByCpu.get(1L));
+        assertEquals(50L, medium.activeTicksByCpu.get(1L));
+        assertEquals(10_000L, medium.tickCostNsByCpu.get(1L));
+    }
+
+    @Test
     void zeroMachineTicksNeverTurnsQueueLatencyIntoCraftTime() {
         XTProfileData.MediumRecord medium = new XTProfileData.MediumRecord();
         XTProfileIntervalUnion all = new XTProfileIntervalUnion();
