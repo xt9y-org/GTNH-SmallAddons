9f1a01f7f89380c7b05fbf9fec78c49bf90ffb27
xt9y <felix@xt9y.de>
2026-09-16 23:21:44 +0200

    test: require minimal XTProfile dispatch context

--- src/test/java/com/xt9y/features/xtprofile/XTProfileMinimalSurfaceTest.java
+++ src/test/java/com/xt9y/features/xtprofile/XTProfileMinimalSurfaceTest.java
@@ -1,19 +1,22 @@
 package com.xt9y.features.xtprofile;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import net.minecraft.tileentity.TileEntity;
 
 import org.junit.jupiter.api.Test;
 
 class XTProfileMinimalSurfaceTest {
 
     @Test
-    void legacyCommandAndBrowserProfilerAreNotPackaged() {
-        assertMissing("com.xt9y.features.xtprofile.XTProfileCommand");
-        assertMissing("com.xt9y.features.xtprofile.XTProfileHttpServer");
-        assertMissing("com.xt9y.features.xtprofile.XTProfileManager");
-    }
-
-    private static void assertMissing(String className) {
-        assertThrows(ClassNotFoundException.class, () -> Class.forName(className));
+    void dispatchContextOnlyNeedsTheResolvedTarget() {
+        TileEntity target = new TileEntity();
+        XTProfileDispatchContext.begin(null, null);
+        try {
+            XTProfileDispatchContext.recordTarget(target);
+            assertSame(target, XTProfileDispatchContext.target());
+        } finally {
+            XTProfileDispatchContext.end();
+        }
     }
 }
