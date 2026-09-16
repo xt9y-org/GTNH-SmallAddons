637b2e2c25ada8db72a7b17c6d47e6e46ad90b89
xt9y <felix@xt9y.de>
2026-09-16 15:39:17 +0200

    feat: hook AE2 returned outputs

--- src/mixin/java/com/xt9y/features/mixin/mixins/late/MixinCraftingCPUClusterXTProfile.java
+++ src/mixin/java/com/xt9y/features/mixin/mixins/late/MixinCraftingCPUClusterXTProfile.java
@@ -4,6 +4,8 @@ import net.minecraft.inventory.InventoryCrafting;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
@@ -12,6 +14,7 @@ import com.xt9y.features.xtprofile.XTProfileRouteTracker;
 
 import appeng.api.networking.crafting.ICraftingMedium;
 import appeng.api.networking.crafting.ICraftingPatternDetails;
+import appeng.api.storage.data.IAEStack;
 import appeng.me.cluster.implementations.CraftingCPUCluster;
 
 @Mixin(value = CraftingCPUCluster.class, remap = false)
@@ -37,5 +40,10 @@ public abstract class MixinCraftingCPUClusterXTProfile {
         } finally {
             XTProfileDispatchContext.end();
         }
+    }
+
+    @Inject(method = "recordReturnedOutputs", at = @At("HEAD"))
+    private void xtprofile$returnedOutput(IAEStack<?> returnedStack, CallbackInfo ci) {
+        XTProfileRouteTracker.INSTANCE.recordReturnedOutput((CraftingCPUCluster) (Object) this, returnedStack);
     }
 }
